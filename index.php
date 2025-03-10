128b23a7dea08216f087ec4bbd049347847c2c79
KOKILAVANI_M <kokilavanikokilavani904@gmail.com>
2025-03-10 20:38:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <style>
+    body
+    {
+    background-image:url('https://scx2.b-cdn.net/gfx/news/hires/2019/harassment.jpg');
+    background-size:cover;
+    }
+    </style>  
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            <h1>Women Harassment Reporting System</h1>
+        </div>
+        <div id="wrapper">
+<p>The Women Harassment Reporting System (WHRS) is a revolutionary online
+platform designed to provide a safe and secure environment for women to reportincidents of harassment. 
+With WHMS, women can report incidents of harassment , and our team of experts will 
+work tirelessly to ensure that their cases are handled with confidentiality and sensitivity.
+Our website is user-friendly, allowing women to quickly and easily report incidents of harassment.
+Our online reporting system is available 24/7.Our website is to provide a safe and secure space for women 
+to report incidents of harassment, and to connect with others who have experienced similar situations.
+If you or someone you know has experienced harassment, we encourage you to visit our website and report 
+the incident.</p>
+        </div>
+        <div id="navi">
+           <ul>
+            <li><a href="userlogin.php">User Login</a></li>
+            <li><a href="adminlogin.php">Admin Login</a></li>
+            <li><a href="total.php">Total Complaints</a></li>
+           </ul> 
+        </div>
+        <div id="footer">
+            <p>Copyright &copy; Vaani2005 </p>
+        </div>
+    </div>
+</body>
+</html>
